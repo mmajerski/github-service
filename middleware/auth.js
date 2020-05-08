ea4f6a76cc988e41b88ef7b7c253121e941d60aa
userq11 <unlimited5511@interia.pl>
2020-05-08 21:00:31 +0200

    add profile experience route

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
 
   if (!token) {
-    return res.status(401).json({ msg: "No token, authoriation denied" });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   // verify token
