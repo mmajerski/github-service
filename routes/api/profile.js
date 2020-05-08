086124efd8ea3436170bde4a227cc18e1c6e49ab
userq11 <unlimited5511@interia.pl>
2020-05-08 19:00:40 +0200

    get user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("./../../middleware/auth");
 
-// @desc    Test
-// @route   GET /api/profile
-// @access  Public
-router.get("/", (req, res) => res.send("profile router"));
+const Profile = require("./../../models/Profile");
+const User = require("./../../models/User");
+
+// @desc    Get current users profile
+// @route   GET /api/profile/me
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ req: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
