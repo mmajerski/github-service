d599f9ae3fa157e457cb702a4cd5d6f61438538e
userq11 <unlimited5511@interia.pl>
2020-05-22 20:40:22 +0200

    deploy prepare

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require(path);
 
 const app = express();
 
@@ -8,16 +9,21 @@ connectDB();
 // parsing to json
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
-
 // routes
 app.use("/api/visits", require("./routes/api/visits"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
