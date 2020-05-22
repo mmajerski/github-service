35d7325d0851e0d1b7a46c3cc18abf3766cad23a
userq11 <unlimited5511@interia.pl>
2020-05-22 17:48:53 +0200

    add posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST
+} from "./types";
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -20,7 +26,7 @@ export const getPosts = () => async (dispatch) => {
 
 export const deletePost = (postId) => async (dispatch) => {
   try {
-    await axios.delete(`/api/posts/posts/${postId}`);
+    await axios.delete(`/api/posts/${postId}`);
 
     dispatch({
       type: DELETE_POST,
@@ -28,6 +34,30 @@ export const deletePost = (postId) => async (dispatch) => {
     });
 
     dispatch(setAlert("Post Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+export const addPost = (formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  try {
+    const res = await axios.post("/api/posts", formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Post Created", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -17,6 +17,7 @@ export const RESET_PROFILE_LOADING = "RESET_PROFILE_LOADING";
 export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
 export const GET_POSTS = "GET_POSTS";
 export const DELETE_POST = "DELETE_POST";
+export const ADD_POST = "ADD_POST";
 export const POST_ERROR = "POST_ERROR";
 export const UPDATE_LIKES = "UPDATE_LIKES";
 export const GET_VISITED = "GET_VISITED";
